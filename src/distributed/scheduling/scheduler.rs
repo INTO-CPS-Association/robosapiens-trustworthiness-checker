INTO-CPS-Association/robosapiens-trustworthiness-checker

commit a025f026f71d911434e8bd0bf1a8ed132b9730d0

Fixes to MAPE-K scheduling loop based on Morten's comments

src/distributed/scheduling/scheduler.rs
@@ -120,7 +120,7 @@ impl Scheduler {
             let should_plan = match self.replanning_condition {
                 ReplanningCondition::ConstraintsFail => !constraints_hold,
                 ReplanningCondition::Always => true,
-                ReplanningCondition::Never => plan.is_none(),
+                ReplanningCondition::Never => false,
             };
             if should_plan {
                 if !self.suppress_output {
@@ -145,7 +145,7 @@ impl Scheduler {
             }
 
             if !self.suppress_output {
-                info!("Monitor");
+                info!("MAPE-K iteration end");
             }
         }
 

commit b627afe226befa96af576f9f0eb9695e835bd353

Fix bug causing distributed tests to hang introduced in a025f02 and reenable the tests

src/distributed/scheduling/scheduler.rs
@@ -120,7 +120,9 @@ impl Scheduler {
             let should_plan = match self.replanning_condition {
                 ReplanningCondition::ConstraintsFail => !constraints_hold,
                 ReplanningCondition::Always => true,
-                ReplanningCondition::Never => false,
+                // Even if replanning is disabled, we need to plan at least once
+                // so we have an initial plan
+                ReplanningCondition::Never => plan.is_none(),
             };
             if should_plan {
                 if !self.suppress_output {

tests/test_distributed.rs
@@ -19,7 +19,6 @@ use trustworthiness_checker::{
 };
 use winnow::Parser;
 
-#[ignore = "Ignoring distributed tests due to hangs"]
 #[test(apply(smol_test))]
 async fn test_distributed_at_stream(executor: Rc<LocalExecutor<'static>>) {
     let x: OutputStream<Value> = Box::pin(stream::iter(vec![1.into(), 2.into(), 3.into()]));
@@ -77,7 +76,6 @@ async fn test_distributed_at_stream(executor: Rc<LocalExecutor<'static>>) {
     assert_eq!(output[2], vec![true.into(), 4.into(), 5.into()]);
 }
 
-#[ignore = "Ignoring distributed tests due to hangs"]
 #[test(apply(smol_test))]
 async fn test_distributed_dist_spec_1(executor: Rc<LocalExecutor<'static>>) {
     let x: OutputStream<Value> = Box::pin(stream::iter(vec![1.into(), 2.into(), 3.into()]));
@@ -135,7 +133,6 @@ async fn test_distributed_dist_spec_1(executor: Rc<LocalExecutor<'static>>) {
     assert_eq!(output[2], vec![0.into(), 4.into(), 5.into()]);
 }
 
-#[ignore = "Ignoring distributed tests due to hangs"]
 #[test(apply(smol_test))]
 async fn test_distributed_dist_spec_2(executor: Rc<LocalExecutor<'static>>) {
     let x: OutputStream<Value> = Box::pin(stream::iter(vec![1.into(), 2.into(), 3.into()]));
@@ -193,7 +190,6 @@ async fn test_distributed_dist_spec_2(executor: Rc<LocalExecutor<'static>>) {
     assert_eq!(output[2], vec![2.into(), 4.into(), 5.into()]);
 }
 
-#[ignore = "Ignoring distributed tests due to hangs"]
 #[test(apply(smol_test))]
 async fn test_distributed_dist_spec_3(executor: Rc<LocalExecutor<'static>>) {
     let x: OutputStream<Value> = Box::pin(stream::iter(vec![1.into(), 2.into(), 3.into()]));
@@ -251,7 +247,6 @@ async fn test_distributed_dist_spec_3(executor: Rc<LocalExecutor<'static>>) {
     assert_eq!(output[2], vec![1.into(), 4.into(), 5.into()]);
 }
 
-#[ignore = "Ignoring distributed tests due to hangs"]
 #[test(apply(smol_test))]
 async fn test_distributed_dist_spec_4(executor: Rc<LocalExecutor<'static>>) {
     let x: OutputStream<Value> = Box::pin(stream::iter(vec![1.into(), 2.into(), 3.into()]));