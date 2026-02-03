11b16939e4529be048edef4674d3dd4e0b0d89f4
Thomas Wright <tom.tdw@gmail.com>
2026-02-03 11:18:04 +0100

    Add partial stream casting functions for handling NoVal and Deferred values

--- src/core/stream_casting.rs
+++ src/core/stream_casting.rs
@@ -1,4 +1,6 @@
-use crate::{OutputStream, Value, core::StreamData};
+use crate::{
+    OutputStream, Value, core::StreamData, lang::dynamic_lola::type_checker::PartialStreamValue,
+};
 use futures::StreamExt;
 use std::fmt::Debug;
 
@@ -22,4 +24,24 @@ pub fn from_typed_stream<T: Into<Value> + StreamData>(
     stream: OutputStream<T>,
 ) -> OutputStream<Value> {
     Box::pin(stream.map(|x| x.into()))
+}
+
+pub fn to_typed_partial_stream<T: TryFrom<Value, Error = ()> + Debug>(
+    stream: OutputStream<Value>,
+) -> OutputStream<PartialStreamValue<T>> {
+    Box::pin(stream.map(|x| match x {
+        Value::NoVal => PartialStreamValue::NoVal,
+        Value::Deferred => PartialStreamValue::Deferred,
+        x => PartialStreamValue::Known(x.try_into().expect("Type error when casting stream")),
+    }))
+}
+
+pub fn from_typed_partial_stream<T: Into<Value> + StreamData>(
+    stream: OutputStream<PartialStreamValue<T>>,
+) -> OutputStream<Value> {
+    Box::pin(stream.map(|x| match x {
+        PartialStreamValue::NoVal => Value::NoVal,
+        PartialStreamValue::Deferred => Value::Deferred,
+        PartialStreamValue::Known(value) => value.into(),
+    }))
 }
